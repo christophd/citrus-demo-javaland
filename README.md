1736e5f16cb688fad6c20acce5c3eb7a0ff2a077
christophd <deppisch@consol.de>
2016-02-24 16:06:13 +0100

    Initial commit testing basic employee registry on Wildfly 10

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
-# citrus-demo-javaland
-"Arquillian &amp; Citrus" JavaLand 2016 demo code
+Demo for "Arquillian &amp; Citrus" JavaLand 2016 talk ![Logo][1]
+==============
+
+Arquillian &amp; Citrus JavaLand Demo
+---------
+
+The employee registry demo application is based on a javaee 7 sample application and manages a list of employees. Each employee has a name and an age property. In addition to that you can
+specify optional email address when creating a new employee. The automatic welcome mail message should be sent out to the mocked Citrus mail server then.
+
+The application exposes a Http REST API for adding, listing and deleting employees. Further more the application is listening for JMS messages on an ActiveMQ message broker.
+
+Using the demo
+---------
+
+This sample is using wildfly application server which is managed by Arquillian. The wildfly distribution is automatically loaded and unzipped during the Maven build.
+The Arquillian test deploys the employee registry application as micro web archive deployment and runs all integration tests. Following command should do the trick:
+
+```
+mvn clean verify
+```
+
+You should see several tests cases reporting success at the end of the Maven build.
+
+Information
+---------
+
+For more information on Arquillian see [www.arquillian.org][4].
+
+For more information on Citrus see [www.citrusframework.org][2], including
+a complete [reference manual][3].
+
+ [1]: http://www.citrusframework.org/images/brand_logo.png "Citrus"
+ [2]: http://www.citrusframework.org
+ [3]: http://www.citrusframework.org/reference/html/
+ [4]: http://www.arquillian.org
---
+++ src/main/java/com/consol/citrus/demo/javaland/EmployeeResource.java
@@ -0,0 +1,74 @@
+/*
+ * Copyright 2016 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.consol.citrus.demo.javaland;
+
+import com.consol.citrus.demo.javaland.model.Employee;
+import com.consol.citrus.demo.javaland.model.Employees;
+
+import javax.ejb.EJB;
+import javax.enterprise.context.RequestScoped;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+
+@Path("employee")
+@RequestScoped
+public class EmployeeResource {
+
+   @EJB
+   private EmployeeRepository repository;
+
+   @GET
+   @Produces({ "application/xml", "application/json" })
+   public Employees getList() {
+      return repository.getEmployees();
+   }
+
+   @GET
+   @Produces({ "application/json", "application/xml" })
+   @Path("{id}")
+   public Employee get(@PathParam("id") int id) {
+      if (id < repository.getEmployees().getEmployees().size())
+         return repository.getEmployees().getEmployees().get(id);
+      else
+         return null;
+   }
+
+   @POST
+   @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+   public void addToList(@FormParam("name") String name,
+                         @FormParam("age") int age, @FormParam("email") String email) {
+      repository.addEmployee(new Employee(name, age, email));
+   }
+
+   @PUT
+   public void putToList(@FormParam("name") String name,
+                         @FormParam("age") int age, @FormParam("email") String email) {
+      addToList(name, age, email);
+   }
+
+   @DELETE
+   public void deleteAll() {
+      repository.deleteEmployees();
+   }
+
+   @DELETE
+   @Path("{name}")
+   public void deleteFromList(@PathParam("name") String name) {
+      repository.deleteEmployee(name);
+   }
+
+}
